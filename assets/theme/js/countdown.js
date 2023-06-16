e06ddc53f395d36fa623b7a1c0aae2ce2ecd5cd8
Kevin Kuan <jhkuan1989@hotmail.com>
2023-06-16 23:06:17 +0800

    add countdown

---
+++ assets/theme/js/countdown.js
@@ -0,0 +1,26 @@
+// Set the date and time for the countdown (adjust this according to your needs)
+var countdownDate = new Date("September 30, 2023 18:00:00").getTime();
+
+// Update the countdown every second
+var countdown = setInterval(function() {
+  // Get the current date and time
+  var now = new Date().getTime();
+
+  // Calculate the time remaining
+  var timeRemaining = countdownDate - now;
+
+  // Calculate days, hours, minutes, and seconds
+  var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  // Display the countdown in the "countdown" element
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the countdown is finished, display a message
+  if (timeRemaining < 0) {
+    clearInterval(countdown);
+    document.getElementById("countdown").innerHTML = "Countdown has ended!";
+  }
+}, 1000);
